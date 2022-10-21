12ad50719356f311d5448686e0c8fad23f353647
Brayan Vargas <103152474+BrayanAntonio08@users.noreply.github.com>
2022-10-21 14:40:32 -0600

    Update IF4100_Proyecto_CreacionTablas.sql
    Se crean algunas tablas de respectiva a las personas y edificación

--- IF4100_Proyecto_CreacionTablas.sql
+++ IF4100_Proyecto_CreacionTablas.sql
@@ -1,35 +1,83 @@
 
 CREATE SCHEMA Person
-
+/*
 Create table Person.Person(
 	person_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	name varchar(100) NOT NULL,
 	phone_number varchar(50) UNIQUE NOT NULL,
 	adress varchar (255) NOT NULL
 )
+Create table Person.Owner(
+	owner_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	person_id INT UNIQUE NOT NULL,
+	CONSTRAINT fk_owner_person 
+		FOREIGN KEY (person_id)
+		REFERENCES Person.Person(person_id)
+)
+Create table Person.Tenant(
+	tenant_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	person_id INT UNIQUE NOT NULL,
+	CONSTRAINT fk_tenant_person 
+		FOREIGN KEY (person_id)
+		REFERENCES Person.Person(person_id)
+)
+Create table Person.Patient(
+	patient_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	person_id INT UNIQUE NOT NULL,
+	reported_illnesses varchar(150) NOT NULL,
+	reported_surgeries varchar(150) NOT NULL,
+	CONSTRAINT fk_patient_person 
+		FOREIGN KEY (person_id)
+		REFERENCES Person.Person(person_id)
+)
+*/ --tables are already created
 
-CREATE SCHEMA Place
+--CREATE SCHEMA Place
 
+Create table Place.Building(
+	code INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	service varchar(200) NOT NULL
+)
 CREATE table Place.Endowment(
-	CODE GENERATED AS IDENTITY PRIMARY KEY ,
+	code INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY ,
 	service_type varchar(200) not null,
 	price Integer not null
+)
+Create table Place.Room(
+	building_code INT,
+	room_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+	number int not null,
+	floor int not null,
+	CONSTRAINT pk_number_at_floor PRIMARY KEY(number,floor),
+	lenght int not null,
+	width int not null,
+	windows bit not null,
+	is_consultory bit not null,
+	is_rented bit not null,
+	administration_costs int not null,
+	rental_date datetime not null,
+	last_remodeling datetime not null,
+	owner_id int not null,
+	tenant_it itn not null,
+	CONSTRAINT fk_owner_of_room 
+		FOREING KEY(owner_id)
+		REFERENCES Person.Owner(owner_id),
+	CONSTRAINT fk_tenant_of_room 
+		FOREING KEY(tenant_it)
+		REFERENCES Person.Tenant(tenant_it)
 )
 
 CREATE SCHEMA Staff
 
+
+
 CREATE SCHEMA Service
 
-
-Create table service(
-code Integer primary key,
-service_type varchar(200)
+Create table Service.Service(
+	code INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	service_type varchar(200) UNIQUE NOT NULL
 )
-Create table edificio(
-code Integer primary key,
-service varchar(200)
-)
-Create table medicine(
-medicine_id Integer primary key,
-name varchar(200)
+Create table Service.Medicine(
+	medicine_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name varchar(200) UNIQUE NOT NULL
 )
